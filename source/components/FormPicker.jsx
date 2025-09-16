53c7b3ea87ee9be26fa2f297da90c89f59719aab
FabinhoDeveloper <fabioezequiel555@gmail.com>
2025-09-16 14:31:40 -0300

    FormPicker estilizado

--- source/components/FormPicker.jsx
+++ source/components/FormPicker.jsx
@@ -1,16 +1,21 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 export default function FormPicker(props) {
     return (
         <View style={styles.container}>
             <Text style={styles.name}>{props.name}</Text>
-            <Picker style={styles.formPicker}>
-                <Picker.Item label="Selecione uma categoria" value="" />
-                <Picker.Item label="Elogio" value="elogio" />
-                <Picker.Item label="Sugestão" value="sugestao" />
-                <Picker.Item label="Reclamação" value="reclamacao" />
-            </Picker>
+                <View style={styles.pickerContainer}>
+                    <Picker 
+                        {...props}
+                    >
+                        <Picker.Item label="Selecione uma categoria" value="" />
+                        <Picker.Item label="Elogio" value="elogio" />
+                        <Picker.Item label="Sugestão" value="sugestao" />
+                        <Picker.Item label="Reclamação" value="reclamacao" />
+                    </Picker>
+                </View>
+                
         </View>
     )
 }
@@ -22,9 +27,20 @@ const styles = StyleSheet.create({
     name: {
         fontSize: 14,
         fontFamily: 'Poppins_500Medium',
-    },  
-    formPicker: {
+    },
+    pickerContainer: {
+        height: 41.31,
         borderWidth: 1,
-        borderColor: 'black'
+        borderColor: '#C4C4C4',
+        borderRadius: 12,
+        overflow: 'hidden',
+        justifyContent: 'center'
+    },
+    formPicker: {
+        marginLeft: 10,
+        height: 41.31,
+        color: "#636364",
+        fontFamily: 'Poppins_300Light',
+        fontSize: 14,
     }
 })
