bd6811cf5371a0f05b3a67a722ec328bc72f0947
FabinhoDeveloper <fabioezequiel555@gmail.com>
2025-09-18 09:10:30 -0300

    Primeira estruturação das páginas de agendamento de consulta

---
+++ source/screens/ConsultationConfirmation.jsx
@@ -0,0 +1,11 @@
+import SecondaryStackHeader from "../components/SecondaryStackHeader";
+import ScrollViewWithMarginBottom from "../components/ScrollViewWithMarginBottom";
+
+export default function ConsultationConfirmation() {
+    return (
+        <ScrollViewWithMarginBottom size={100}>
+            <SecondaryStackHeader title={"Agendamento de Consulta"} color={"#228D9C"}/>
+            
+        </ScrollViewWithMarginBottom>
+    )
+}
---
+++ source/screens/ConsultationMainPage.jsx
@@ -0,0 +1,12 @@
+import SecondaryStackHeader from "../components/SecondaryStackHeader";
+import ScrollViewWithMarginBottom from "../components/ScrollViewWithMarginBottom";
+
+export default function ConsultationMainPage() {
+    return (
+        <ScrollViewWithMarginBottom size={100}>
+            <SecondaryStackHeader title={"Agendamento de Consulta"} color={"#228D9C"}/>
+            
+        </ScrollViewWithMarginBottom>
+    )
+
+}
