c2d361fe4ba140a37c60072135974a817eff7fd6
FabinhoDeveloper <fabioezequiel555@gmail.com>
2025-09-22 09:30:25 -0300

    Componente de mensagem de sucesso adicionado na tela de confirmacao de consulta

--- source/screens/ConsultationSuccess.jsx
+++ source/screens/ConsultationSuccess.jsx
@@ -1,30 +1,13 @@
-import { View, StyleSheet, Text } from 'react-native'
-import Feather from '@expo/vector-icons/Feather';
+import { View, StyleSheet } from 'react-native'
 
 import SecondaryStackHeader from '../components/SecondaryStackHeader'
-import NeutralButton from '../components/NeutralButton';
-import { useNavigation } from '@react-navigation/native';
+import SuccessMessage from '../components/SuccessMessage';
 
 export default function ConsultationSuccess() {
-    const navigation = useNavigation()
-
     return (
         <View style={styles.container}>
             <SecondaryStackHeader title={"Agendamento\nde Consulta"} color={"#228D9C"}/>
-            <View style={styles.mainContent}>
-                <View style={styles.confirmationIcon}>
-                    <Feather name="check" size={56} color="white" />
-                </View>
-
-                <Text style={styles.mainTitle}>{"Recebemos sua\nsolicitação"}</Text>
-
-                <Text style={styles.mainText}>
-                    Fique tranquilo, sua solicitação foi registrada com sucesso.
-                    Nossa equipe entrará em contato para confirmar os detalhes do seu atendimento.
-                </Text>
-                
-                <NeutralButton text={"Voltar"} onPress={() => navigation.navigate("ServicesNavigation")}/>
-            </View>
+            <SuccessMessage text={"Fique tranquilo, sua solicitação foi registrada com sucesso. Nossa equipe entrará em contato para confirmar os detalhes do seu atendimento."}/>
         </View>
     )
 }
@@ -32,30 +15,5 @@ export default function ConsultationSuccess() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    },
-    mainContent: {
-        height: '60%',
-        marginHorizontal: 20,
-        gap: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    confirmationIcon: {
-        backgroundColor: '#3E7F0C',
-        height: 103,
-        width: 103,
-        borderRadius: 60,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    mainTitle: {
-        fontSize: 24,
-        textAlign: 'center',
-        fontFamily: 'Poppins_500Medium'
-    },
-    mainText: {
-        fontSize: 14,
-        textAlign: 'center',
-        fontFamily: 'Poppins_500Medium'
     }
 })
