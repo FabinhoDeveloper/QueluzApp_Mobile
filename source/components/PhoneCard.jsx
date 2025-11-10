9104e9e9584717aa16d35d89d7780856a7748c12
FabinhoDeveloper <fabioezequiel555@gmail.com>
2025-11-10 15:25:05 -0300

    Tela de telefones uteis desenhada

---
+++ source/components/PhoneCard.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Linking } from "react-native";
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function PhoneCard({ name, address, phone }) {
+    const [isOpened, setIsOpened] = useState(false) 
+    
+    return (
+        <TouchableOpacity style={styles.container} onPress={() => setIsOpened(!isOpened)}>
+            <View style={styles.titleArea}>
+                <Text style={styles.title}>{name}</Text>
+                <Ionicons name="chevron-down" size={20} color="#0C447F" />
+            </View>
+            {isOpened && (
+                <View style={styles.informationArea}>
+                    <Text style={styles.informationText}>{address}</Text>
+                    <TouchableOpacity style={styles.phoneButton}>
+                        <Text onPress={() => Linking.openURL(`tel:${phone}`)} style={styles.phoneText}>{phone}</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        </TouchableOpacity>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 0.5,
+        borderColor: 'rgba(0,0,0,0.25)',
+        width: 333,
+    },
+    titleArea: {
+        height: 50,
+        backgroundColor: '#FFF',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        flexDirection: 'row'
+    },
+    title: {
+        fontFamily: 'Poppins_600SemiBold'
+    },
+    informationArea: {
+        justifyContent: 'center',
+        height: 64,
+        backgroundColor: '#F0F0F0',
+        borderLeftWidth: 2,
+        borderRightWidth: 2,
+        borderColor: '#0C447F',
+        paddingHorizontal: 20
+    },
+    informationText: {
+        fontFamily: 'Poppins_300Light',
+        fontSize: 12,
+        color: '#878787'
+    },
+    phoneButton: {
+        borderRadius: 10,
+        backgroundColor: '#3E7F0C',
+        height: 25,
+        width: '40%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    phoneText: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 12,
+        color: '#FFF',
+        textAlignVertical: 'center', // Android
+        paddingTop: 0, // Remove padding top padrão
+        paddingBottom: 0, // Remove padding bottom padrão
+        includeFontPadding: false, // Remove espaçamento extra da fonte (Android)
+    }
+})
--- source/screens/UsefulPhoneNumbers.jsx
+++ source/screens/UsefulPhoneNumbers.jsx
@@ -1,12 +1,59 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import SecondaryTabHeader from "../components/SecondaryTabHeader";
+import PhoneCard from "../components/PhoneCard";
+import ScrollViewWithMarginBottom from "../components/ScrollViewWithMarginBottom";
+
 
 export default function UsefulPhoneNumbers() {
     return (
-        <View style={{ flex: 1 }}>
+        <ScrollViewWithMarginBottom size={150}>
             <SecondaryTabHeader/>
-            <Text>UsefulPhoneNumbers Screen</Text>
-        </View>
+            <View style={styles.container}>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Emergência (SAMU)"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+                <PhoneCard name={"Prefeitura Municipal"} address={"Rua Prudente de Moraes, 100, Centro"} phone={"(12) 3147-9020"}/>
+            </View>
+        </ScrollViewWithMarginBottom>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        paddingTop: 50
+    }
+})
