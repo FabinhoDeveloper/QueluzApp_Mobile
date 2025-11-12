a2a298e1d1f0e78b922620933d72d2317a31888e
FabinhoDeveloper <fabioezequiel555@gmail.com>
2025-11-12 10:13:52 -0300

    Login required adicionado na tela de consultas

--- source/screens/ConsultationMainPage.jsx
+++ source/screens/ConsultationMainPage.jsx
@@ -4,27 +4,34 @@ import SecondaryStackHeader from "../components/SecondaryStackHeader";
 import ScrollViewWithMarginBottom from "../components/ScrollViewWithMarginBottom";
 import SecondaryButton from "../components/SecondaryButton";
 import FormSection from "../components/FormSection";
-import TripCard from "../components/TripCard";
 import ConsultationCard from "../components/ConsultationCard";
+import LoginRequired from '../components/LoginRequired'
+import { useAuth } from "../contexts/AuthContext";
 
 export default function ConsultationMainPage() {
+    const { user } = useAuth()
+
     return (
         <ScrollViewWithMarginBottom size={100}>
             <SecondaryStackHeader title={"Agendamento\nde Consulta"} color={"#228D9C"}/>
-            <View style={styles.mainContent}>
-                <FormSection marginBottom={true}>
-                    <SecondaryButton title={"Agendar consulta"} linkTarget={"ConsultationForm"}/>
-                </FormSection>
+            { user ? (
+                <View style={styles.mainContent}>
+                    <FormSection marginBottom={true}>
+                        <SecondaryButton title={"Agendar consulta"} linkTarget={"ConsultationForm"}/>
+                    </FormSection>
 
-                <FormSection title={"Minhas consultas"}>
-                    <View style={styles.tripRecords}>
-                        <ConsultationCard status={"Consulta agendada"} specialistType={"Oftalmologista"} local={"ESF - Palha"} data={"02/02/2025"}/>
-                        <ConsultationCard status={"Consulta cancelada"} specialistType={"Endocrinologista"} local={"ESF - Palha"} data={"02/02/2025"}/>
-                        <ConsultationCard status={"Aguardando vaga"} specialistType={"Ginecologista"} local={"ESF - Palha"} data={"02/02/2025"}/>
-            
-                    </View>
-                </FormSection>
-            </View>
+                    <FormSection title={"Minhas consultas"}>
+                        <View style={styles.tripRecords}>
+                            <ConsultationCard status={"Consulta agendada"} specialistType={"Oftalmologista"} local={"ESF - Palha"} data={"02/02/2025"}/>
+                            <ConsultationCard status={"Consulta cancelada"} specialistType={"Endocrinologista"} local={"ESF - Palha"} data={"02/02/2025"}/>
+                            <ConsultationCard status={"Aguardando vaga"} specialistType={"Ginecologista"} local={"ESF - Palha"} data={"02/02/2025"}/>
+                
+                        </View>
+                    </FormSection>
+                </View>
+            ) : (
+                <LoginRequired/>
+            ) }
         </ScrollViewWithMarginBottom>
     )
 }
